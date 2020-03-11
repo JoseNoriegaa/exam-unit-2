93e27551ddc1c81f16dfd4226172aba7baea439b
Jose Noriega <josenoriega723@gmail.com>
2020-03-10 19:48:15 -0700

    add clear function after capture

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"strconv"
 
+	"github.com/josenoriegaa/exam-unit-2/helpers"
+
 	structs "github.com/josenoriegaa/exam-unit-2/structs"
 )
 
@@ -13,7 +15,7 @@ func main() {
 		student.UUID = strconv.Itoa(i)
 		student.Save()
 	}
-
+	helpers.Clear()
 	for i := 0; i < 5; i++ {
 		student := structs.Student{}
 		student.UUID = strconv.Itoa(i)
